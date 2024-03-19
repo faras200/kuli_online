ed84224cf3cbc2dc9f55229b1efd2ccb1f3d889c
faras200 <farasaldi200@gmail.com>
2024-03-19 17:34:40 +0700

    Update Dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,23 +2,80 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Carbon\Carbon;
 use App\Models\Tag;
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
+use App\Models\Transaction;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 // use App\Helpers\NavigationHelper;
 class DashboardController extends Controller
 {
     public function index()
     {
+        $kuli = User::whereHas('roles', function ($query) {
+            $query->where('name', 'kuli');
+        })->get();
+
+        $transaction = Transaction::all();
+
+        $date10 = Carbon::today()->subDays(30)->tz('Asia/Jakarta');
+
+        $tenDaysAgo = $date10;
+        $tenDaysAgo->settings(['formatFunction' => 'translatedFormat']);
+
+        $date_array = [];
+        $data_date = [];
+
+        for ($i = 0; $i < 31; $i++) {
+
+            $date_array[] = $tenDaysAgo->format('d.m');
+            $data_date[] = $tenDaysAgo->format('d F Y');
+
+
+            $tenDaysAgo->addDay();
+        }
+
+        // return $data_date[array_key_first($data_date)];
+        $date10 = Carbon::today()->subDays(30)->tz('Asia/Jakarta');
+        $charttrx['kuli'] = Transaction::selectRaw('DATE_FORMAT(transactions.tanggal, "%d.%m") as date, COALESCE(SUM(transactions.salary), 0) as total')
+            ->whereDate("transactions.tanggal", '>=', $date10)
+            ->groupBy('date')
+            ->get()
+            ->pluck('total', 'date');
+
+        $bulan_terakhir = Carbon::now()->subMonths(11);
+        $bulan = [];
+        $data_bulan_amount = [];
+        $data_bulan_count = [];
+
+        for (
+            $i = 0;
+            $i < 12;
+            $i++
+        ) {
+            $bulan_sekarang = $bulan_terakhir->copy()->addMonths($i);
+            $bulan[] = $bulan_sekarang->format('M Y');
+
+            $data_bulan_amount[] = Transaction::selectRaw('COALESCE(SUM(transactions.salary), 0) as total')
+                ->whereDate("transactions.tanggal", '>=', $bulan_sekarang->format('Y-m-1'))
+                ->whereDate("transactions.tanggal", '<', $bulan_sekarang->copy()->addMonth()->format('Y-m-1'))
+                ->first()->total;
+        }
+
         $data = [
             'page_title'        => 'Dashboard',
-            'total_posts'       => Post::getTotalRows(),
-            'total_categories'  => Category::getTotalRows(),
-            'total_tags'        => Tag::getTotalRows(),
-            'total_users'       => User::getTotalRows(),
+            'total_transactions'        => $transaction->count(),
+            'total_users'       => $kuli->count(),
+            'labels'            => $date_array,
+            'charttrx'          => $charttrx,
+            'charttrxbulan'     => $data_bulan_amount,
+            'bulans'            => $bulan
         ];
+
+        // return $data;
 
         return view('backend/dashboard/index', $data);
     }
--- resources/views/backend/dashboard/record.blade.php
+++ resources/views/backend/dashboard/record.blade.php
@@ -1,56 +1,124 @@
 <div class="row">
-    <div class="col-lg-3 col-12">
-        <div class="small-box bg-success">
-            <div class="inner">
-                <h3>{{ $total_posts }}</h3>
-                <p>Total Post</p>
-            </div>
-            <div class="icon">
-                <i class="fas fa-newspaper"></i>
-            </div>
-            <a href="{{ route('posts.index') }}" class="small-box-footer">Detail <i
-                    class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <div class="col-lg-3 col-12">
-        <div class="small-box bg-warning">
-            <div class="inner">
-                <h3>{{ $total_categories }}</h3>
-                <p>Total Category</p>
-            </div>
-            <div class="icon">
-                <i class="fas fa-tag"></i>
-            </div>
-            <a href="{{ route('categories.index') }}" class="small-box-footer">Detail <i
-                    class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-
-    <div class="col-lg-3 col-12">
+    <div class="col-lg-6 col-12">
         <div class="small-box bg-primary">
             <div class="inner">
-                <h3>{{ $total_tags }}</h3>
-                <p>Total Tags</p>
+                <h3>{{ $total_transactions }}</h3>
+                <p>Total Transaction</p>
             </div>
             <div class="icon">
                 <i class="fas fa-tags"></i>
             </div>
-            <a href="{{ route('tags.index') }}" class="small-box-footer">Detail <i
+            <a href="{{ route('transactions.index') }}" class="small-box-footer">Detail <i
                     class="fas fa-arrow-circle-right"></i></a>
         </div>
     </div>
 
-    <div class="col-lg-3 col-12">
+    <div class="col-lg-6 col-12">
         <div class="small-box bg-danger">
             <div class="inner">
                 <h3>{{ $total_users }}</h3>
-                <p>Total User</p>
+                <p>Total Kuli</p>
             </div>
             <div class="icon">
                 <i class="fas fa-users"></i>
             </div>
-            <a href="{{ route('users.index') }}" class="small-box-footer">Detail <i
+            <a href="{{ route('kuli.index') }}" class="small-box-footer">Detail <i
                     class="fas fa-arrow-circle-right"></i></a>
         </div>
     </div>
 </div>
+<hr>
+<div class="row">
+    <div class="col-12">
+        <div class="loading-overlay" id="loadingBarCount">
+            <div class="loading-spinner"></div>
+        </div>
+        <canvas id="bar-chart-total-count" width="1000" height="450"></canvas>
+    </div>
+</div>
+<hr>
+<div class="row">
+    <div class="col-12">
+        <div class="loading-overlay" id="loadingBarAmount">
+            <div class="loading-spinner"></div>
+        </div>
+        <canvas id="bar-chart-total-amount" width="1000" height="450"></canvas>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script type="text/javascript">
+    var labels = {{ Js::from($labels) }};
+    var bulans = {{ Js::from($bulans) }};
+    window.onload = function() {
+        // showLoading('loadingBarCount');
+        var charttrx = {{ Js::from($charttrx) }}
+        var colorPalette = ['red', 'blue', 'green', 'yellow', 'purple', 'orange'];
+        var Data = processDataset("Transaksi", charttrx, colorPalette);
+
+        var Chart_bar = new Chart(document.getElementById("bar-chart-total-count"), {
+            type: 'line',
+            data: Data,
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Transaksi Kuli 30 Hari Terakhir'
+                    }
+                }
+            }
+        });
+
+        var charttrxbulan = {{ Js::from($charttrxbulan) }}
+        var Chart_bar_amount = new Chart(document.getElementById("bar-chart-total-amount"), {
+            type: 'bar',
+            data: {
+                labels: bulans,
+                datasets: [{
+                    label: "Transaksi",
+                    backgroundColor: "#00a950",
+                    data: charttrxbulan,
+                    borderRadius: 5,
+                }, ]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Total Transaksi 12 Bulan Terakhir'
+                    }
+                }
+            }
+        });
+    };
+
+    function processDataset(dataset, data, colors) {
+        var color = -1;
+        var result = [];
+        $.each(data, function(index, value) {
+            var innerArray = [];
+            $.each(labels, function(index2, value2) {
+                const val = value[value2];
+                innerArray.push(val || 0);
+            });
+            result.push({
+                label: dataset + " " + index,
+                data: innerArray,
+                borderColor: colors[color += 1 % colors.length]
+            });
+        });
+        return {
+            labels: labels,
+            datasets: result
+        };
+    }
+</script>
